49c6edaade982e39663ae2b069491d1c248a1132
MushroomyJr <mustafa.the.almighty@gmail.com>
2023-09-10 16:52:06 -0400

    adding to progress made to Sept 7

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import ExpesneItem from "./components/ExpenseItem";
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Gym",
+      amount: 55.85,
+      date: new Date(2023, 8, 1),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h1>Expenses!</h1>
+      <ExpesneItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpesneItem>
+      <ExpesneItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpesneItem>
+      <ExpesneItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpesneItem>
+      <ExpesneItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpesneItem>
     </div>
   );
 }
