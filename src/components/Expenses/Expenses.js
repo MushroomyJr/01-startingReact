35fe122bdfc7e34504366e036562742fcc1ff23a
MushroomyJr <mustafa.the.almighty@gmail.com>
2023-10-18 13:04:48 -0400

    changed state handling from app.js to expenses.js and added all the year filtering

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,16 +1,21 @@
 import ExpesneItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
-//import { useState } from "react";
+import { useState } from "react";
 import Card from "../UI/Card";
 import "./Expenses.css";
 const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState(2020);
   const yearFilter = (yearChosen) => {
-    props.yearFilter(yearChosen);
+    setFilteredYear(yearChosen);
   };
+  const filteredExpenses = props.items.filter((expense) => {
+    //eslint-disable-next-line
+    return expense.date.getFullYear() == filteredYear;
+  });
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={props.yearPicked} onChangeFilter={yearFilter} />
-      {props.items.map((expense) => (
+      <ExpensesFilter selected={filteredYear} onChangeFilter={yearFilter} />
+      {filteredExpenses.map((expense) => (
         <ExpesneItem
           key={expense.id}
           title={expense.title}
